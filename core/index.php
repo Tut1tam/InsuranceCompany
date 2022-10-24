1a317975603d7aa1ea6d694c8f20713d826e511b
Tut1tam <dolzhich.maxim@yandex.ru>
2022-10-24 15:53:08 +0300

    first commit

---
+++ core/index.php
@@ -0,0 +1,9 @@
+<html lang="ru">
+<head>
+    <title>Hello world page</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body>
+    <div class="hello">Hello world</div>
+</body>
+</html>
