c22541d886d1653270c2bb8a6e23f21fdc3481f8
Tut1tam <dolzhich.maxim@yandex.ru>
2022-11-22 16:48:31 +0300

    DELETE index.php/style.css

--- core/index.php
+++
@@ -1,9 +0,0 @@
-<html lang="ru">
-<head>
-    <title>Hello world page</title>
-    <link rel="stylesheet" href="style.css" type="text/css"/>
-</head>
-<body>
-    <div class="hello">Hello world</div>
-</body>
-</html>
